73418f3b4e0aada91f90d6e2005c78f9cf65c2a3
sapquiz <chrberelocation@gmail.com>
2025-05-05 20:56:05 +0200

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Quiz from './Quiz.svelte';
+</script>
+
+<main>
+  <h1>Willkommen zum SAP Quiz!</h1>
+  <Quiz />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+  }
+</style>
